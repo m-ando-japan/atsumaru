7cde26e9664216a21dfdf024d20b1a6ac4519360
青野 知比古 <>
2019-06-15 18:45:08 +0900

    test

--- hosting/src/components/CreateEvent.vue
+++ hosting/src/components/CreateEvent.vue
@@ -24,10 +24,7 @@
             @paste="updateTags"
           ></v-combobox>
         </v-flex>
-
-        <v-flex xs12>    
-          <v-btn block color="success" to="/View">投稿</v-btn>  
-        </v-flex>
+        <v-btn block color="success">投稿</v-btn>      
       </v-layout>
     </v-container>
     <v-flex xs12 sm6 md3>  
